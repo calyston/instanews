b0c5d1447e9e3a899991740aed5e0ef0b8f1f148
unknown <calyston.14@gmail.com>
2020-02-17 13:11:46 -0500

    feat: limit results to 12

--- javascript/main.js
+++ javascript/main.js
@@ -15,13 +15,19 @@ $(function () {
       .done(function (data) {
         console.log(data.results[0].multimedia)
 
+        let i = 0;
         $.each(data.results, function (index, value) {
-          console.log(`${index}: ${value}`)
+          //console.log(`${index}: ${value}`)
+
           let articleUrl = value.url
           let abstract = value.abstract
           let articleImage = value.multimedia[0].url
 
-          $(".grid").append(`<a href="${articleUrl}"><figure><img src="${articleImage}"><p>${abstract}</p></figure></a>`)
+          if (value.multimedia.length !== 0 && i < 12) {
+            i++;
+            $(".grid").append(`<a href="${articleUrl}"><figure><img src="${articleImage}"><p>${abstract}</p></figure></a>`);
+            return i !== 12;
+          }
         })
       })
   })
